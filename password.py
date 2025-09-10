d7d4293a3e42a0bc4d9eb27c17ee35b608ac42fe
Juhász Bálint <JuhaszBalint@pg.local>
2025-09-10 10:46:44 +0200

    without comments

--- password.py
+++ password.py
@@ -1,11 +1,6 @@
 username = input ("Please enter your username \n")
 password = input ("Please enter your password \n")
-#if username == "Bálint" and password == "abc" or username == "Tami" and password == "123":
 if ((username == "Bálint" and password == "abc") or (username == "Tami" and password == "123")):
     print(f"Your username and password are correct")
-
-#elif username != "Bálint" and password != "abc" or username != "Tami" and password != "123":
-else: #((username != "Bálint" and password != "abc") or (username != "Tami" and password != "123")):
+else: 
     print("either your username or password is incorrect ")
-
-# #means failure
