056c33d23185255ccc02a25491947dd68d41a6ba
Juhász Bálint <JuhaszBalint@pg.local>
2025-09-10 10:45:52 +0200

    working password system with previous mistakes

---
+++ 02_adattípusok.py
@@ -0,0 +1,20 @@
+"""
+Adattípusok
+1.0
+"""
+name = "Juhász Bálint"
+age = "17" 
+height = 1.72 #in meters
+is_student = True
+print (f"name: {name} \n age: {age} \n height: {height} \n is student: {is_student}") #prefereable for me
+
+#print (f"""name: {name},
+#        age: {age},
+#        height: {height},
+#        is student: {is_student},""")
+
+#type(name)
+print (type (name))
+print (type (age))
+print (type (is_student))
+print (type (height))
